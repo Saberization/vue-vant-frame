ed88c9e303195e1f73725c9301b9c546bd81fe67
Tohsaka Rin <saberization@qq.com>
2019-03-01 17:54:17 +0800

    update button

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,5 +1,25 @@
 <template>
-
+  <van-button
+    :type="type"
+    :size="size"
+    :text="text"
+    :tag="tag"
+    :native-type="nativeType"
+    :block="block"
+    :plain="plain"
+    :square="square"
+    :round="round"
+    :disabled="disabled"
+    :loading="loading"
+    :loading-text="loadingText"
+    :loading-size="loadingSize"
+    :url="url"
+    :to="to"
+    :replace="replace"
+    @click="onClickBtn"
+  >
+    <slot></slot>
+  </van-button>
 </template>
 
 <script>
@@ -9,6 +29,64 @@ export default {
   name: 'Button',
   components: {
     'van-button': Button
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'primary'
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    text: String,
+    tag: {
+      type: String,
+      default: 'button'
+    },
+    nativeType: String,
+    block: {
+      type: Boolean,
+      default: false
+    },
+    plain: {
+      type: Boolean,
+      default: false
+    },
+    square: {
+      type: Boolean,
+      default: false
+    },
+    round: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    loadingText: String,
+    loadingSize: {
+      type: String,
+      default: '20px'
+    },
+    url: String,
+    to: {
+      default: null
+    },
+    replace: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onClickBtn() {
+      this.$emit('click')
+    }
   }
 }
 </script>
--- src/showcase/button.vue
+++ src/showcase/button.vue
@@ -1,14 +1,50 @@
 <template>
+  <div class="container">
+    <van-header
+      title="Button 按钮"
+      left-text="返回"
+      :left-arrow="true"
+    ></van-header>
 
+    <div class="van-content">
+      <h4 class="cell-title">按钮类型</h4>
+      <div class="btn-area">
+        <van-button type="default">默认按钮</van-button>
+        <van-button type="primary">主要按钮</van-button>
+        <van-button type="info">信息按钮</van-button>
+        <van-button type="warning">警告按钮</van-button>
+        <van-button type="danger">危险按钮</van-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { Button } from 'vant'
+import Header from '@components/header'
+import Button from '@components/button'
 
 export default {
   name: 'Button',
   components: {
-    'van-button': Button
+    'van-button': Button,
+    'van-header': Header
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.van-content {
+  padding-left: 10px;
+  padding-right: 10px;
+
+  .cell-title {
+    padding-left: 0;
+  }
+
+  .btn-area {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+}
+</style>
