07d37b169d62a8ef1684b5ace1e3fb0568c6e1b6
Tohsaka Rin <saberization@qq.com>
2019-03-08 13:58:07 +0800

    update switchcell

---
+++ src/components/switchcell.vue
@@ -0,0 +1,78 @@
+<template>
+  <van-switch-cell
+    v-model="value"
+    :title="title"
+    :loading="loading"
+    :disabled="disabled"
+    :size="size"
+    :active-color="activeColor"
+    :inactive-color="inactiveColor"
+    :active-value="activeValue"
+    :inactive-value="inactiveValue"
+    @change="onChange"
+  ></van-switch-cell>
+</template>
+
+<script>
+import { SwitchCell } from 'vant'
+
+export default {
+  name: 'SwitchCell',
+  components: {
+    [SwitchCell.name]: SwitchCell
+  },
+  model: {
+    prop: 'switchValue',
+    event: 'changeValue'
+  },
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: String,
+      default: '24px'
+    },
+    activeColor: {
+      type: String,
+      default: '#1989fa'
+    },
+    inactiveColor: {
+      type: String,
+      default: '#fff'
+    },
+    activeValue: {
+      default: true
+    },
+    inactiveValue: {
+      default: false
+    },
+    switchValue: {
+      default: false
+    }
+  },
+  data () {
+    return {
+      value: false
+    }
+  },
+  methods: {
+    onChange (checked) {
+      this.$emit('changeValue', checked)
+      this.$emit('change', checked)
+    }
+  },
+  created () {
+    this.value = this.switchValue
+  }
+}
+</script>
---
+++ src/showcase/switchcell.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <van-header
+      title="SwitchCell 开关单元格"
+      left-arrow
+    ></van-header>
+    <div class="van-content">
+      <h4 class="cell-title">基础用法</h4>
+      <van-cell-group>
+        <van-switch-cell
+          v-model="checked"
+          title="标题"
+          @change="onChange"
+        />
+      </van-cell-group>
+
+      <h4 class="cell-title">禁用状态</h4>
+      <van-cell-group>
+        <van-switch-cell
+          v-model="checked"
+          title="标题"
+          disabled
+        />
+      </van-cell-group>
+
+      <h4 class="cell-title">加载状态</h4>
+      <van-cell-group>
+        <van-switch-cell
+          v-model="checked"
+          title="标题"
+          loading
+        />
+      </van-cell-group>
+    </div>
+  </div>
+</template>
+
+<script>
+import vanHeader from '@components/header'
+import vanSwitchCell from '@components/switchcell'
+import vanCellGroup from '@components/cellgroup'
+import { Toast } from 'vant'
+
+export default {
+  name: 'SwitchCell',
+  components: {
+    vanHeader,
+    vanSwitchCell,
+    vanCellGroup
+  },
+  data () {
+    return {
+      checked: true
+    }
+  },
+  methods: {
+    onChange (checked) {
+      Toast(`当前选中状态 ${checked}`)
+    }
+  }
+}
+</script>
