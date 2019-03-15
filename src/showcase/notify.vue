c2dd19439e0eb9dacb16f674f46e3e61191d852b
Tohsaka Rin <saberization@qq.com>
2019-03-15 17:49:57 +0800

    update notify

--- src/showcase/notify.vue
+++ src/showcase/notify.vue
@@ -7,7 +7,6 @@
     <div class="van-content">
       <h4 class="cell-title">基础用法</h4>
       <van-button @click="showNotify('通知内容')">显示消息通知</van-button>
-
       <h4 class="cell-title">自定义配置</h4>
       <van-button @click="showNotify({message: '通知内容', duration: 1000, background: '#1989fa'})">显示自定义消息通知</van-button>
     </div>
