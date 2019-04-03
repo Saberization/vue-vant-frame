7e21ec2ad6a77bf8b97d7cf205a45f6a43600bd3
Tohsaka Rin <saberization@qq.com>
2019-04-03 10:23:36 +0800

    update sha256

--- src/showcase/util.vue
+++ src/showcase/util.vue
@@ -4,11 +4,13 @@
 
 <script>
 import sha1 from '@utils/sha1'
+import sha256 from '@utils/sha256'
 
 export default {
   name: 'Util',
   created () {
     console.log(sha1.hex('123123'))
+    console.log(sha256.hex('123123'))
   }
 }
 </script>
