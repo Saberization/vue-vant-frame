5d13e9fa380c36cbf839d1baf0caa74ef6a1c7d1
Tohsaka Rin <saberization@qq.com>
2019-05-21 18:00:39 +0800

    rewrite pulltorefresh

--- src/showcase/pulltorefresh.vue
+++ src/showcase/pulltorefresh.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="contaienr">
+  <div class="container">
     <van-header title="Pulltorefresh 下拉刷新" left-arrow></van-header>
     <div class="van-content">
-      <div ref="mescroll" class="mescroll">
-        <div>
-          <van-cell v-for="(item, index) in listdata" :key="index" :title="item.title"></van-cell>
+      <div id="minirefresh" class="minirefresh-wrap">
+        <div class="minirefresh-scroll">
+          <div class="data-list" ref="listdata">
+            <van-cell v-for="(item, index) in listdata" :key="index" :title="item.title"></van-cell>
+          </div>
         </div>
       </div>
     </div>
@@ -14,8 +16,8 @@
 <script>
 import vanHeader from '@components/header';
 import vanCell from '@components/cell';
-import MeScroll from 'mescroll.js';
-import 'mescroll.js/mescroll.min.css';
+import MiniRefreshTools from 'minirefresh';
+import 'minirefresh/dist/debug/minirefresh.css';
 import Util from '@utils';
 
 export default {
@@ -26,82 +28,64 @@ export default {
   },
   data () {
     return {
-      mescroll: null,
-      listdata: []
+      miniRefresh: null,
+      listdata: [],
+      requestDelayTime: 600,
+      pageindex: 0
     };
   },
   methods: {
-    upCallback (page) {
+    requestAjax (eventType) {
+      if (eventType === 'down') {
+        this.pageindex = 0;
+      }
+
       Util.ajax({
         url: 'http://115.29.151.25:8012/request.php?action=testV7List',
         data: JSON.stringify({
           token: 'RXBvaW50X1dlYlNlcml2Y2VfKiojIzA2MDE=',
           params: {
-            pageindex: page.num,
-            pagesize: 10,
+            pageindex: this.pageindex,
+            pagesize: 7,
             keyword: 'type1'
           }
         }),
         success: ({ custom }) => {
           const infolist = custom.infolist || [];
+          const len = infolist.length;
 
-          console.log('upCallback');
-
-          if (Array.isArray(infolist) && infolist.length >= 1) {
-            infolist.forEach(e => {
-              this.listdata.push(e);
-            });
+          if (len > 0) {
+            if (eventType === 'down') {
+              this.listdata = infolist;
+              this.miniRefresh.endDownLoading(true);
+            } else {
+              infolist.forEach(e => {
+                this.listdata.push(e);
+              });
+            }
           }
-
-          this.$nextTick(() => {
-            this.mescroll.endSuccess(infolist.length, infolist.length >= 1);
-          });
         },
-        error: () => {
-          this.mescroll.endErr();
-        }
-      });
-    },
-    downCallback () {
-      Util.ajax({
-        url: 'http://115.29.151.25:8012/request.php?action=testV7List',
-        data: JSON.stringify({
-          token: 'RXBvaW50X1dlYlNlcml2Y2VfKiojIzA2MDE=',
-          params: {
-            pageindex: 0,
-            pagesize: 10,
-            keyword: 'type1'
-          }
-        }),
-        success: ({ custom }) => {
-          const infolist = custom.infolist || [];
-
-          this.listdata = infolist;
-          setTimeout(() => {
-            this.mescroll.endSuccess();
-          }, 600);
-        },
-        error: () => {
-          this.mescroll.endErr();
+        error: (err) => {
+          console.error(err);
         }
       });
     }
   },
   mounted () {
-    this.mescroll = new MeScroll(this.$refs.mescroll, {
-      up: {
-        callback: this.upCallback,
-        page: {
-          num: 0,
-          size: 7
-        },
-        htmlNodata: '<p class="van-list__finished-text">没有更多数据了</p>'
-      },
+    this.miniRefresh = new MiniRefresh({
+      container: '#minirefresh',
       down: {
-        callback: this.downCallback,
-        htmlNodata: '<p class="van-list__finished-text">没有更多数据了</p>'
+        callback: () => {
+          this.requestAjax('down');
+        }
+      },
+      up: {
+        isAuto: true,
+        callback: () => {
+          this.requestAjax('up');
+        }
       }
-    });
+    }); 
   }
 };
 </script>
