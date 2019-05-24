600a2dc568c5485a623bfab3e9b186bca58f4c17
Tohsaka Rin <saberization@qq.com>
2019-05-24 11:06:49 +0800

    update modulepage

--- src/pages/module/module.vue
+++ src/pages/module/module.vue
@@ -2,24 +2,13 @@
   <div class="container">
     <h1>Module Page</h1>
     <div class="van-content">
-      <van-button @click="openPage">跳转至 Module2</van-button>
     </div>
   </div>
 </template>
 
 <script>
-import vanButton from '@components/button';
-
 export default {
-  name: 'ModuleName',
-  components: {
-    vanButton
-  },
-  methods: {
-    openPage () {
-      ejs.page.open('./module2');
-    }
-  }
+  name: 'ModuleName'
 };
 </script>
 
