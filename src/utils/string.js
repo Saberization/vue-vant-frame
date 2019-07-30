73b62d03bffc207972523cd8e333f3433990937b
saberization <saberization@qq.com>
2019-07-30 15:08:55 +0800

    更新正则验证规则

--- src/utils/string.js
+++ src/utils/string.js
@@ -104,41 +104,161 @@ export default {
   },
 
   /**
-   * 判断一个字符串是否是手机号码
-   * @param {String} str 要检验的字符串
-   * 正则：手机号（精确）
-   * <p>移动：134(0-8)、135、136、137、138、139、147、150、151、152、157、158、159、178、182、183、184、187、188、198</p>
-   * <p>联通：130、131、132、145、155、156、175、176、185、186、166</p>
-   * <p>电信：133、153、173、177、180、181、189、199</p>
-   * <p>全球星：1349</p>
-   * <p>虚拟运营商：170</p>
-   * @return {Boolean} true or false
+   * 2019-06-13 14:05:46
+   * 验证是否是数字
+   * @param {String} v 要验证的数字或字符串数字
+   * @returns {Boolean} 是否是数字或者字符串数字
    */
-  isPhoneNum(str) {
-    if (!str) {
-      return str;
+  isFloat(v) {
+    if (Util.isNull(v) || v === '') {
+      return true;
     }
 
-    return /^(86)?1(3|4|5|6|7|8|9)\d{9}$/.test(str);
+    var n = String(v);
+
+    if (n.indexOf('-') !== -1 && n.indexOf('-') !== 0) {
+      return false;
+    }
+
+    n = n.replace('-', '');
+
+    if (n.split('.').length > 2) {
+      return false;
+    }
+
+    return n.length > 0 && !(/[^0-9.]/).test(n) && !(n.charAt(n.length - 1) === '.');
   },
 
   /**
-   * 判断一个字符串是否是手机号码或者是座机号码
-   * @param {String} str 要检验的字符串
-   * @return {Boolean} true or false
+   * 2019-06-13 14:06:59
+   * 手机号码格式验证
+   * @param {String} v 手机号码
+   * @returns {Boolean} 是否是手机号码
    */
-  isTelNum(str) {
-    if (!str) {
-      return str;
+  isMobile(v) {
+    if (Util.isNull(v) || v === '') {
+      return true;
     }
 
-    /**
-     * 这是固定电话的正则
-     * 区号 前面一个0，后面跟2-3位数字，区号后面可以加 - 也可以不加
-     * 电话号 5-8位数字，不以0开头
-     * 分机号 一般都是3位数字，我们认为大于等于3位，小于8位
-     */
-    return this.isPhoneNum(str) || /^(0\d{2,3}-?|\(0\d{2,3}\))?[1-9]\d{4,7}(-\d{3,8})?$/.test(str);
+    return (/^1[3456789]\d{9}$/).test(v);
+  },
+
+  /**
+   * 2019-06-13 14:09:51
+   * 固定电话号码验证
+   * @param {String} v 固定电话号码
+   * @returns {Boolean} 是否是固定电话号码
+   */
+  isTel(v) {
+    if (Util.isNull(v) || v === '') {
+      return true;
+    }
+
+    return (/^0\d{2,3}-?\d{7,8}$/).test(v) || (/^\(0\d{2,3}\)\d{7,8}$/).test(v);
+  },
+
+  /**
+   * 2019-06-13 14:15:29
+   * 电话号码格式验证
+   * @param {String} v 电话号码
+   * @returns {Boolean} 是否是电话号码
+   */
+  isPhone(v) {
+    if (Util.isNull(v) || v === '') {
+      return true;
+    }
+
+    return (/^1[3456789]\d{9}$/).test(v) || (/^0\d{2,3}-?\d{7,8}$/).test(v) || (/^\(0\d{2,3}\)\d{7,8}$/).test(v);
+  },
+
+  /**
+   * 2019-06-13 14:15:29
+   * 邮政编码格式验证
+   * @param {String} v 邮政编码
+   * @returns {Boolean} 是否是邮政编码
+   */
+  isPostCode(v) {
+    if (Util.isNull(v) || v === '') {
+      return true;
+    }
+
+    return (/^\d{6}$/).test(v);
+  },
+
+  /**
+   * 2019-06-13 14:15:29
+   * 组织机构代码
+   * @param {String} v 组织机构代码
+   * @returns {Boolean} 是否是组织机构代码
+   */
+  isOrgCode(v) {
+    if (Util.isNull(v) || v === '') {
+      return true;
+    }
+
+    // 组织机构代码
+    if ((/^[A-Z0-9]{8}-[A-Z0-9]$/).test(v)) {
+      return true;
+    }
+
+    // 统一社会信用代码
+    var reg = /^([0-9A-Z]{2})([0-9]{6})([0-9A-Z]{10})$/;
+
+    if (!reg.test(v)) {
+      return false;
+    }
+
+    return true;
+  },
+
+  /**
+   * 身份证验证
+   * @param {String} v 身份证
+   * @returns {Boolean} 是否是身份证
+   */
+  isIdCard(v) {
+    if (Util.isNull(v) || v === "")
+      return true;
+
+    function validateIdCard(idCard) {
+      // 15位和18位身份证号码的正则表达式
+      var regIdCard = /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/;
+
+      // 如果通过该验证，说明身份证格式正确，但准确性还需计算
+      if (regIdCard.test(idCard)) {
+        if (idCard.length == 18) {
+          const idCardWi = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2); // 将前17位加权因子保存在数组里
+          const idCardY = new Array(1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2); // 这是除以11后，可能产生的11位余数、验证码，也保存成数组
+          let idCardWiSum = 0; // 用来保存前17位各自乖以加权因子后的总和
+          for (var i = 0; i < 17; i++) {
+            idCardWiSum += idCard.substring(i, i + 1) * idCardWi[i];
+          }
+
+          const idCardMod = idCardWiSum % 11; // 计算出校验码所在数组的位置
+          const idCardLast = idCard.substring(17); // 得到最后一位身份证号码
+
+          // 如果等于2，则说明校验码是10，身份证号码最后一位应该是X
+          if (idCardMod == 2) {
+            if (idCardLast === "X" || idCardLast === "x") {
+              return true;
+            } else {
+              return false;
+            }
+          } else {
+            // 用计算出的验证码与最后一位身份证号码匹配，如果一致，说明通过，否则是无效的身份证号码
+            if (idCardLast == idCardY[idCardMod]) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        }
+      } else {
+        return false;
+      }
+    }
+
+    return validateIdCard(v);
   },
 
   /**
